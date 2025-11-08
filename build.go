a7e38d2f709093a6907e78e99a307055b469b7fe
Patrick Gaskin <patrick@pgaskin.net>
2025-11-08 03:31:04 -0500

    Add build tests
    Also work around odd bug(?) with chunked build. If I can't figure it
    out before the I do the next release, I'm going to remove it.

--- build.go
+++ build.go
@@ -102,6 +102,22 @@ func (t *Trie) BuildWeights(keys iter.Seq2[string, float32], cfg Config) error {
 			copy(tmp[8:], key)
 			cnum++
 		} else {
+			// TODO: why the fuck does a key larger than the chunk size break
+			// tests if I don't do this?!? it literally doesn't even make a
+			// difference if I do this before or after, and I've already
+			// confirmed that the memory allocations are being done properly and
+			// don't overlap, that the keys match in Go and WebAssembly, and
+			// that all the keys are being seen... the breakage seems to happen
+			// when the case where it fits in a chunk is interspersed with the
+			// case where it doesn't
+			if !internal.NoChunkBuild && cptr != 0 {
+				//fmt.Println(csz, chunkSize, len(cbuf))
+				if _, err := mod.Call("marisa_build_push_chunk", uint64(cptr), uint64(cnum)); err != nil {
+					return err
+				}
+				cptr, cbuf = 0, nil
+			}
+
 			ptr, buf, err := mod.Alloc(len(key))
 			if err != nil {
 				return err
